3bf884d00738149dbfc93db4a27a6f0b16e94a1f
eileenschaubert <eileen.schaubert@gmail.com>
2023-12-12 14:48:09 -0600

    Remove notes

--- js/script.js
+++ js/script.js
@@ -2,14 +2,6 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
-
-
-// const getData = async function () {
-//     const res = await fetch("https://api.quotable.io/quotes?author=maya-angelou");
-//     const data = await res.json();
-//     console.log(data);
-//   };
-//   getData();
   
 const getImage = async function() {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
